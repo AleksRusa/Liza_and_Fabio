fba0012403704ff5450ad49ff22a90ea9b2038dc
AleksRusa <rusakovmen@gmail.com>
2025-02-17 20:33:03 +0300

    made base structure, and tested html page

--- src/main.py
+++ src/main.py
@@ -4,19 +4,26 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+from routers import router
 import uvicorn
-import routers
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://127.0.0.1:8000",
+#         "http://localhost:8000",
+#     ],
+# )
 
-app.include_router(routers.router)
+app.include_router(router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "src.main:app",
+        reload=True,
+        port=8000,
+    )
--- src/routers.py
+++ src/routers.py
@@ -1,9 +1,16 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.templating import Jinja2Templates
 
 from models import Vote
 from config import votes
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory="src/templates")
+
+@router.get("/")
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @router.post("/vote")
 async def vote(vote: Vote):
